361366d8cc6dcf1fac70afd960d429816073cfed
Yasmin <ykasem84@gmail.com>
2025-12-17 16:39:27 +0200

    cleaning the code

--- ieee task.py
+++ ieee task.py
@@ -1,27 +1,27 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> #1.Mean is the sum of values divided by count,Use for symmetric data, Median is the middle value after sorting,use when data has extremes,Mode is the value that appears most often,use categorical data or repeated values.
-... #2.P value is the probability of observing the data or something more extreme given the null hypothesis is true.
-... #3.Left means the tail extends to the left. The mean is less than the median,Right means the tail extends to the right. Then the mean is greater.
-... #4.Skewness measures asymmetry of the distribution, Kurtosis measures tail weight and concentration around the center.
-... #5.List uses square brackets [] and is mutable,Tuple uses parentheses () and is immutable.
-... #6.random.random() gives a float,random.randint(1,10) gives an integer,and random.choice(list) picks one item.
-... #7.
-... for x in range(5):
-...     if x == 2:
-...         continue
-...     if x == 4:
-...         break
-...     pass
-...     print(x)
-... 
+
+#1.Mean is the sum of values divided by count,Use for symmetric data, Median is the middle value after sorting,use when data has extremes,Mode is the value that appears most often,use categorical data or repeated values.
+#2.P value is the probability of observing the data or something more extreme given the null hypothesis is true.
+#3.Left means the tail extends to the left. The mean is less than the median,Right means the tail extends to the right. Then the mean is greater.
+#4.Skewness measures asymmetry of the distribution, Kurtosis measures tail weight and concentration around the center.
+#5.List uses square brackets [] and is mutable,Tuple uses parentheses () and is immutable.
+#6.random.random() gives a float,random.randint(1,10) gives an integer,and random.choice(list) picks one item.
+#7.
+for x in range(5):
+    if x == 2:
+        continue
+    if x == 4:
+        break
+    pass
+    print(x)
+
 0
 1
 3
->>> #8.You can reverse a list with the built in reverse method,It changes the list in place.
+#8.You can reverse a list with the built in reverse method,It changes the list in place.
 #9. 
 print(max([2, 7, 3, 9, 5]))
 #10.An outlier is a value that lies far from most other values in the dataset,lets say If heights are near 160 to 180 and one value is 300 that value is an outlier.
 #11. Mean = (3 + 5 + 3 + 4 + 5) ÷ 5 = 20 ÷ 5 = 4 books
 #12. Median = 3, 3, 4, 5, 5 = 4 books 
 #13. Mode = 3 and 5 both appears twice, so there are two modes 
+
